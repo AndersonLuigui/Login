b2e28ff81dbb24575f6ce7c6812cc65463b4e7bd
Anderson Luigui <alns1536@hotmail.com>
2024-08-22 20:13:54 -0300

    Implementação do metod trim() responsável por limpar os dados no input e melhorias no evento de windowlocationhref do widget netlify

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
-
-
-
 const users = [
     { username: 'AndersonLuigui', password: 'senha123' },
     { username: 'Neura', password: 'neura123' },
     { username: 'emanuel', password: '1234' },
-    { username: 'joãomiguel', password: 'joaomiguel123'}
+    { username: 'joãomiguel', password: 'joaomiguel123' }
 ];
 
 const loginForm = document.getElementById('loginForm');
@@ -14,19 +11,16 @@ const loginResult = document.getElementById('loginResult');
 loginForm.addEventListener('submit', function(event) {
     event.preventDefault();
     
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+    const username = loginForm.username.value.trim();  // O Método trim() remove espaços em branco
+    const password = loginForm.password.value.trim();  // O Método trim() remove espaços em branco
 
     const user = users.find(user => user.username === username && user.password === password);
 
     if (user) {
         loginResult.textContent = `Bem-vindo, ${username}!`;
-        localStorage.setItem('username', username);
+        sessionStorage.setItem('username', username);  // O sessionStorage serve para expiração na aba
         window.location.href = './html/page.html';
-
     } else {
         loginResult.textContent = 'Usuário ou senha incorretos.';
     }
 });
-
-
